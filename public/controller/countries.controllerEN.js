fe204af42ca20f0c2e14466ff4ff491224f7026f
Jeremias ramirez <jereramirez499@gmail.com>
2020-01-29 09:56:39 -0400

    quitando servicio y refactorizando

--- public/controller/countries.controllerEN.js
+++ public/controller/countries.controllerEN.js
@@ -1,8 +1,43 @@
-app.controller("en", ["$scope", "$routeParams", function($scope, $routeParams) {
+app.controller("en", ["$scope", "$http", function($scope, $http) {
 
-    $scope.choose = "klk en";
-
+    // back page
     $scope.backButton = function() {
         window.history.back()
     }
+
+    $scope.countriesEN = [];
+
+
+
+
+    // http
+
+    var getData = function() {
+
+        let urlS = 'https://restcountries.eu/rest/v2/lang/en'
+
+
+        $http({
+
+            method: 'get',
+            url: urlS
+
+        }).then(function(response) {
+
+            $scope.countriesEN.push(response)
+
+        })
+
+    }
+
+    getData()
+
+
+
+    // paginador
+
+
+
+
+
 }])
--- public/controller/countries.controllerES.js
+++ public/controller/countries.controllerES.js
@@ -1,8 +1,41 @@
-app.controller("es", ["$scope", "$routeParams", function($scope, $routeParams) {
+app.controller("es", ["$scope", "$http", function($scope, $http) {
 
-
+    // back page
 
     $scope.backButton = function() {
         window.history.back()
     }
+
+
+
+    $scope.countriesES = [];
+
+
+
+
+
+    // http
+    var getData = function() {
+
+        let urlS = 'https://restcountries.eu/rest/v2/lang/es'
+
+        $http({
+
+            method: 'get',
+            url: urlS
+
+        }).then(function(response) {
+
+            $scope.countriesES.push(response)
+
+        })
+
+    }
+
+    getData()
+
+
+    // paginator
+
+
 }])
